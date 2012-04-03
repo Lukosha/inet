f8119bcbc9c62b086818dd7219e067426e68f1e8
Zoltan Bojthe <zbojthe@urdeso.hu>
2012-04-03 17:54:50 +0200

    IPv4NetworkConfigurator dumpConfig: dump linklocal multicast addresses, too -- NEED REVISE

--- src/networklayer/autorouting/ipv4/IPv4NetworkConfigurator.cc
+++ src/networklayer/autorouting/ipv4/IPv4NetworkConfigurator.cc
@@ -1043,19 +1043,14 @@ void IPv4NetworkConfigurator::dumpConfig(IPv4Topology& topology)
             InterfaceEntry *interfaceEntry = interfaceInfo->interfaceEntry;
             IPv4InterfaceData *interfaceData = interfaceEntry->ipv4Data();
             const std::vector<IPv4Address>& multicastAddresses = interfaceData->getJoinedMulticastGroups();
-            bool found = false;
-            for (int k = 0 ; k < (int)multicastAddresses.size(); k++)
-                if (!multicastAddresses[k].isLinkLocalMulticast())
-                    found = true;
-            if (found) {
+            {
                 std::stringstream stream;
                 stream << "   <multicast-group hosts=\"" << interfaceInfo->node->module->getFullPath() << "\" interfaces=\"" << interfaceEntry->getName() << "\" address=\"";
                 for (int k = 0 ; k < (int)multicastAddresses.size(); k++)
                 {
-                    if (k) stream << " ";
                     IPv4Address address = multicastAddresses[k];
-                    if (!address.isLinkLocalMulticast())
-                        stream << address.str();
+                    if (k) stream << " ";
+                    stream << address.str();
                 }
                 stream << "\"/>" << endl;
                 fprintf(f, "%s", stream.str().c_str());
